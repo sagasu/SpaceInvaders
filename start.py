9b94e4038e1a06790559f9210efa82023287bff1
Mateusz Kopij <sagasu@gmail.com>
2020-08-20 10:49:41 +0100

    introducing enemies and bunkers

--- start.py
+++ start.py
@@ -23,13 +23,47 @@ class Ship(pygame.sprite.Sprite):
     def draw(self):
         win.blit(self.image, (self.rect.x, self.rect.y))
 
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([25,25])
+        self.image.fill(white)
+        self.rect = self.image.get_rect()
+
+class Bunker(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([8,8])
+        self.image.fill(green)
+        self.rect = self.image.get_rect()
+
 ship = Ship()
 ship.rect.x = 375
 ship.rect.y = 650
 
+enemy_list = pygame.sprite.Group()
+bunker_list = pygame.sprite.Group()
+
+for row in range(1,6):
+    for column in range(1, 11):
+        enemy = Enemy()
+        enemy.rect.x = 80 + (50 * column)
+        enemy.rect.y = 25 + (50 * row)
+        enemy_list.add(enemy)
+
+for bunk in range(3):
+    for row in range(5):
+        for column in range(10):
+            bunker = Bunker()
+            bunker.rect.x = (50 + (275 * bunk)) + (10 * column)
+            bunker.rect.y = 500 + (10 * row)
+            bunker_list.add(bunker)
+
 def redraw():
     win.fill(black)
     ship.draw()
+    enemy_list.draw(win)
+    bunker_list.draw(win)
     pygame.display.update()
 
 run = True

3750e6809e584cec989d664c8dd685aee7e00276
Mateusz Kopij <sagasu@gmail.com>
2020-08-20 10:12:44 +0100

    ship moves left and right

--- start.py
+++ start.py
@@ -7,20 +7,30 @@ win = pygame.display.set_mode((750,750))
 
 pygame.display.set_caption('Space Invaders')
 
+white = (255, 255, 255)
+green = (0, 255, 0)
+red = (255, 0, 0)
+black = (0,0,0)
+
 class Ship(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([50,25])
         self.image.fill(green)
+        self.rect = self.image.get_rect()
+        self.live = 5
+
+    def draw(self):
+        win.blit(self.image, (self.rect.x, self.rect.y))
+
+ship = Ship()
+ship.rect.x = 375
+ship.rect.y = 650
 
 def redraw():
     win.fill(black)
+    ship.draw()
     pygame.display.update()
-
-white = (255, 255, 255)
-green = (0, 255, 0)
-red = (255, 0, 0)
-black = (0,0,0)
 
 run = True
 
@@ -30,6 +40,13 @@ while run:
         if event.type == pygame.QUIT:
             run = False
     
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT]:
+        ship.rect.x += -10
+
+    if key[pygame.K_RIGHT]:
+        ship.rect.x += 10
+
     redraw()
 
 pygame.quit()
